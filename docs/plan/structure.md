a0111c4de5a7835be03e73fe2429ceed036bb264
Harish Raghavan <ragh02@sevenoaksschool.org>
2026-01-16 15:14:10 +0000

    Start writing code: database + YAML config file

--- docs/plan/structure.md
+++ docs/plan/structure.md
@@ -1,6 +1,18 @@
 # Program structure
+
 ## Function details
-TBC
+**Database functions:**<br>
+Functions that interact with the database
+
 | Function name | Parameters | Output | Purpose |
-| - | - | - | - |
-| file2list | filename (str) | list (list) | Convert a file to a list |
+| ------------- | ---------- | ------ | ------- |
+| init_db | db_name (str) | None | Initialise the database and create required tables if missing |
+| execute_query | query (str) | list (str) | Runs an SQL query on the database |
+
+**Configuration functions:**<br>
+Functions that interact with the YAML config file
+
+| Function name | Parameters | Output | Purpose |
+| ------------- | ---------- | ------ | ------- |
+| load_config_file | filename (str) | config (dictionary) | Reads the YAML config file and converts it into a dictionary. |
+| create_config_file | filename (str, default="config.yml"), config_dict (dict, default="{"version": 1,"database_file": "card_game.db"}") | None | Creates a new YAML config file |
--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+# Copyright (c) 2026 Harish Raghavan. All rights reserved
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Two-player card game
+import sqlite3
+import random
+from libs.col import *
+import os
+import sys
+import yaml
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Initialise the SQLite DB and create tables if they don't exist
+def init_db(db_name):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
 
+    # table 1: users
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users';")
+    result = c.fetchall()
+    if len(result) == 0:
+        c.execute('''CREATE TABLE users (
+                username TEXT,
+                password TEXT
+                )''')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # table 2: leaderboard
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='leaderboard';")
+    result = c.fetchall()
+    if len(result) == 0:
+        c.execute('''CREATE TABLE leaderboard (
+                    username TEXT,
+                    score INTEGER
+                    )''')
+    conn.commit()
+    conn.close()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Execute an SQL query and return a result
+def execute_query(query):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute(query)
+    result = c.fetchall()
+    conn.close()
+    return result
+
+# Loads the YAML config file
+def load_config_file(filepath):
+    if os.path.exists(filepath):
+        with open(filepath, "r") as f:
+            config_dict = yaml.safe_load(f)
+            return config_dict
+    else:
+        return("That file doesn't exist!")
+
+# Creates a new YAML config file
+def create_config_file(filepath="config.yml", config_dict=None):
+    if config_dict is None:
+        config_dict = {
+            "version": 1,
+            "database_file": "card_game.db"
+        }
+    with open(filepath, "w") as f:
+        yaml.dump(config_dict, f)
+
+# Does authentication. Reads the global config file
+def do_authentication(username, password, config_dict):
+    if
